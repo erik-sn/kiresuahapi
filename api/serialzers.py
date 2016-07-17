c959f5953a3e7fefdc2d14a66bc5573b47d1bf3d
kiresuah <kiresuah@gmail.com>
2016-07-17 17:26:01 -0400

    initial commit

---
+++ api/serialzers.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User, Group
+from api.models import ToDo, Markup
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+
+class ToDoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = ('title', 'description', 'created', 'modified')
+
+
+class MarkupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Markup
+        fields = ('id', 'title', 'description', 'text', 'created', 'user', 'modified')
---
+++ api/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url, include
+from api import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+
+    url(r'^(?i)auth/(?P<code>[a-zA-Z0-9]+)/$', views.auth, name='auth'),
+
+    url(r'^(?i)todo/$', views.ToDoList.as_view()),
+    url(r'^(?i)todo/(?P<pk>[0-9]+)/$', views.ToDoDetail.as_view()),
+
+    url(r'^(?i)markup/(?P<id>[0-9]+)/$', views.MarkupList.as_view(), name='markup_list'),
+    url(r'^(?i)markup/(?P<id>[0-9]+)/(?P<pk>[0-9]+)/$', views.MarkupDetail.as_view(), name='markup_get_post_put'),
+]
